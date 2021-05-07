319aebc7687f358ea42ad75d68522bd7634069aa
DineshQA1 <dinesh.vijayan@qa.com>
2021-05-07 12:14:25 +0100

    DV commit domain v1.0

---
+++ src/main/java/com/qa/marvel/domain/Marvel.java
@@ -0,0 +1,46 @@
+package com.qa.marvel.domain;
+
+public class Marvel {
+	
+	private Long id;
+	private String name;
+	private String superpower;
+	private boolean supersoldier;
+	private Long age;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSuperpower() {
+		return superpower;
+	}
+	public void setSuperpower(String superpower) {
+		this.superpower = superpower;
+	}
+	public boolean isSupersoldier() {
+		return supersoldier;
+	}
+	public void setSupersoldier(boolean supersoldier) {
+		this.supersoldier = supersoldier;
+	}
+	public Long getAge() {
+		return age;
+	}
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	
+	
+
+}
+
