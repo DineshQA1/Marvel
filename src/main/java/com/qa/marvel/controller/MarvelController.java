40a21b5bfacc13b7f6512032c2b18579b54a1b2a
DineshQA1 <dinesh.vijayan@qa.com>
2021-05-07 14:23:17 +0100

    controller update 2

--- src/main/java/com/qa/marvel/controller/MarvelController.java
+++ src/main/java/com/qa/marvel/controller/MarvelController.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.marvel.domain.Marvel;
 
 @RestController
 public class MarvelController {
@@ -14,6 +18,13 @@ public class MarvelController {
 	@GetMapping("/getMarvel")
 	public List<Marvel> getMarvel(){
 	return this.marvelList;
-}
+	}
+	
+	@PostMapping("/create")
+	public void createCharacter(@RequestBody Marvel marvel) {
+		this.marvelList.add(marvel);
+	}
 	
 }
+	
+
