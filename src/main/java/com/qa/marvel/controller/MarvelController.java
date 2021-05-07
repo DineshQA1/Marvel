a2bac9d28b7211245a902f752fc299f0dfe12019
DineshQA1 <dinesh.vijayan@qa.com>
2021-05-07 12:38:41 +0100

    DV commit controller v1.0

---
+++ src/main/java/com/qa/marvel/controller/MarvelController.java
@@ -0,0 +1,19 @@
+package com.qa.marvel.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarvelController {
+	
+	private List<Marvel> marvelList = new ArrayList<>();
+	
+	@GetMapping("/getMarvel")
+	public List<Marvel> getMarvel(){
+	return this.marvelList;
+}
+	
+}
