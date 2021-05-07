e30ed87fa18772420da1db10a1c69a540425ff35
DineshQA1 <dinesh.vijayan@qa.com>
2021-05-07 12:50:23 +0100

    controller update

--- src/main/java/com/qa/marvel/controller/MarvelController.java
+++ src/main/java/com/qa/marvel/controller/MarvelController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.marvel.domain.Marvel;
+
 @RestController
 public class MarvelController {
 	
